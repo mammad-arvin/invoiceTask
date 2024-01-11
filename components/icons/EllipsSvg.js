9d5b41cc4ff6bad0492f222044cb63f57891c2da
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2024-01-11 12:00:46 +0330

    icons of side bar added

---
+++ components/icons/EllipsSvg.js
@@ -0,0 +1,23 @@
+const EllipsSvg = () => {
+    return (
+        <svg
+            width="11"
+            height="11"
+            viewBox="0 0 11 11"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+        >
+            <circle
+                id="Ellipse 214"
+                cx="5.75"
+                cy="5.375"
+                r="4"
+                fill="#E52929"
+                stroke="white"
+                stroke-width="2"
+            />
+        </svg>
+    );
+};
+
+export default EllipsSvg;
