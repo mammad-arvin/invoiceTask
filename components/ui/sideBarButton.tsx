283fc35f50c5c26c12651419e7189946726a1373
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2024-01-11 12:43:24 +0330

    some additional code deleted

--- components/ui/sideBarButton.tsx
+++ components/ui/sideBarButton.tsx
@@ -9,28 +9,15 @@ const buttonVariants = cva(
     {
         variants: {
             variant: {
-                default:
-                    "bg-primary text-primary-foreground shadow hover:bg-primary/90",
-                destructive:
-                    "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
-                outline:
-                    "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
-                secondary:
-                    "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
                 ghost: " hover:text-accent-foreground",
-                link: "text-primary underline-offset-4 hover:underline",
             },
             size: {
-                // default: "h-9 ",
                 default:
                     "self-stretch h-12 flex-col justify-center items-center gap-1 flex ",
-                sm: "h-8 rounded-md px-3 text-xs",
-                lg: "h-10 rounded-md px-8",
-                icon: "h-9 w-9",
             },
         },
         defaultVariants: {
-            variant: "default",
+            variant: "ghost",
             size: "default",
         },
     }
