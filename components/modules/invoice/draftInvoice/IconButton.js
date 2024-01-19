80dbc8405d35814413d57775d8afa927131c1b08
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2024-01-19 12:38:38 +0330

    edite button change page to inprogress to edite

--- components/modules/invoice/draftInvoice/IconButton.js
+++ components/modules/invoice/draftInvoice/IconButton.js
@@ -4,11 +4,20 @@ import {
     TooltipContent,
     TooltipTrigger,
 } from "@/components/ui/tooltip";
+import InProgress from "../InProgress/InProgress";
 
-const IconButton = ({ text, icon }) => {
+const IconButton = ({ text, icon, setPage }) => {
+    console.log(icon);
     return (
         <Tooltip>
-            <TooltipTrigger className="my-2 mx-3">{icon}</TooltipTrigger>
+            <TooltipTrigger
+                className="my-2 mx-3"
+                onClick={() =>
+                    icon.type.name === "Edit" && setPage(<InProgress />)
+                }
+            >
+                {icon}
+            </TooltipTrigger>
             <TooltipContent>
                 <p>{text}</p>
                 <TooltipArrow />
