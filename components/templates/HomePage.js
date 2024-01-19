77491d4417cbc9a5c866a75a9d487b1044f8bfdd
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2024-01-19 19:39:44 +0330

    updated

--- components/templates/HomePage.js
+++ components/templates/HomePage.js
@@ -1,9 +1,14 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const HomePage = () => {
     return (
         <div className="w-full h-[400px] flex justify-center items-center text-center ">
-            <Link className='border rounded p-4' href="/invoice/choose-a-template/configuration/new-invoice-page"> go to new invoice page </Link>
+            <Link
+                className="border rounded p-4"
+                href="/invoice/choose-a-template/configuration/new-invoice-page"
+            >
+                Go to new invoice page
+            </Link>
         </div>
     );
 };
