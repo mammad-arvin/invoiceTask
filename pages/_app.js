805c6f4578c84b66f7eb7c2d5ae532e7f33258eb
Mohammad Hadi Arvin <mammad.arvin@gmail.com>
2024-01-19 13:31:06 +0330

    now  finalInvoice has no layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,25 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout/Layout";
+import Providers from "@/Redux/Providers";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+    const router = useRouter();
+
+    if (router.pathname.includes("/finalInvoice")) {
+        return (
+            <Providers>
+                <Component {...pageProps} />;
+            </Providers>
+        );
+    } else {
+    }
+
     return (
-        <Layout>
-            <Component {...pageProps} />
-        </Layout>
+        <Providers>
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+        </Providers>
     );
 }
